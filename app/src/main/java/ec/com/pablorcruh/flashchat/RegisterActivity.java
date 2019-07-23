06195aebcb5b9b3d2fdff7f57d64c1e43e125209
pablorcruh <pablorcruh@gmail.com>
2019-07-23 13:09:05 -0500

    registrar usuario dentro de firebase

--- app/src/main/java/ec/com/pablorcruh/flashchat/RegisterActivity.java
+++ app/src/main/java/ec/com/pablorcruh/flashchat/RegisterActivity.java
@@ -1,9 +1,12 @@
 package ec.com.pablorcruh.flashchat;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -11,16 +14,25 @@ import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String CHAT_PREFS= "chatPrefs";
     private static final String DISPLAY_NAME_KEY = "username";
+    private static final String TAG = RegisterActivity.class.getName();
 
 
     private AutoCompleteTextView mEmailView;
     private AutoCompleteTextView mUsernameView;
     private EditText mPasswordView;
     private EditText mConfirmPasswordView;
+
+    private FirebaseAuth mAuth;
+
 
 
     @Override
@@ -43,6 +55,9 @@ public class RegisterActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
 
     }
 
@@ -75,7 +90,7 @@ public class RegisterActivity extends AppCompatActivity {
         if(cancel){
             focusView.requestFocus();
         }else{
-
+            createFirebaseUser();
         }
 
     }
@@ -84,7 +99,34 @@ public class RegisterActivity extends AppCompatActivity {
         return email.contains("@");
     }
 
-    private boolean isPasswordValid(String Password){
-        return true;
+    private boolean isPasswordValid(String password){
+        String confirmPassword = mConfirmPasswordView.getText().toString();
+        return confirmPassword.equals(password) && password.length()>6;
     }
+
+    public void createFirebaseUser(){
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "onComplete: "+task.isSuccessful());
+                        if(!task.isSuccessful()){
+                            Log.d(TAG, "user creation failed: ");
+                        }
+                    }
+                });
+
+    }
+
+    public void showErrorDialog(String message){
+        new AlertDialog.Builder(this)
+                .setTitle("Opps")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok,null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
 }
